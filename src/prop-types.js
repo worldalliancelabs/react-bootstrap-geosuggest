d4d692a9cbc3468f0f9ca35a066b0fa6b1d52ad0
Sergey Gornostaev <svgornostaev@gmail.com>
2018-05-05 12:11:04 +0300

    Update React

--- src/prop-types.js
+++ src/prop-types.js
@@ -1,36 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Default values
  */
 export default {
-  fixtures: React.PropTypes.array,
-  initialValue: React.PropTypes.string,
-  placeholder: React.PropTypes.string,
-  disabled: React.PropTypes.bool,
-  className: React.PropTypes.string,
-  inputClassName: React.PropTypes.string,
-  location: React.PropTypes.object,
-  radius: React.PropTypes.oneOfType([
-    React.PropTypes.string,
-    React.PropTypes.number
+  fixtures: PropTypes.array,
+  initialValue: PropTypes.string,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+  inputClassName: PropTypes.string,
+  location: PropTypes.object,
+  radius: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
   ]),
-  bounds: React.PropTypes.object,
-  country: React.PropTypes.string,
-  types: React.PropTypes.array,
-  queryDelay: React.PropTypes.number,
-  googleMaps: React.PropTypes.object,
-  onSuggestSelect: React.PropTypes.func,
-  onFocus: React.PropTypes.func,
-  onBlur: React.PropTypes.func,
-  onChange: React.PropTypes.func,
-  skipSuggest: React.PropTypes.func,
-  getSuggestLabel: React.PropTypes.func,
-  autoActivateFirstSuggest: React.PropTypes.bool,
-  style: React.PropTypes.shape({
-    input: React.PropTypes.object,
-    suggests: React.PropTypes.object,
-    suggestItem: React.PropTypes.object
+  bounds: PropTypes.object,
+  country: PropTypes.string,
+  types: PropTypes.array,
+  queryDelay: PropTypes.number,
+  googleMaps: PropTypes.object,
+  onSuggestSelect: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  skipSuggest: PropTypes.func,
+  getSuggestLabel: PropTypes.func,
+  autoActivateFirstSuggest: PropTypes.bool,
+  style: PropTypes.shape({
+    input: PropTypes.object,
+    suggests: PropTypes.object,
+    suggestItem: PropTypes.object
   }),
-  ignoreTab: React.PropTypes.bool
+  ignoreTab: PropTypes.bool
 };
